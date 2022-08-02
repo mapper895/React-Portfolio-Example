cb260609bada5f02f9a51dcb6f9d38e3667682c2
mapper895 <mapper.895.mp@gmail.com>
2022-08-02 18:23:47 -0500

    Page Loader

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,6 +5,7 @@ import './index.scss'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -62,6 +63,7 @@ const Home = () => {
         </div>
         <Logo />
       </div>
+      <Loader type="pacman" />
     </>
   )
 }
