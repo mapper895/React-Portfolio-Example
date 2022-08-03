e7a2ee1dab56725df2d997e08e69a489afe54437
mapper895 <mapper.895.mp@gmail.com>
2022-08-02 19:13:57 -0500

    Contact Page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'gmail',
+        'template_YeJhZkgb',
+        refForm.current,
+        'your-token'
+      )
+      .then(
+        () => {
+          alert('Message succesfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, plase try again')
+        }
+      )
+  }
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    name="message"
+                    placeholder="Message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Slobodan Gajic,
+          <br />
+          Servia,
+          <br />
+          Branka RadiCevica 19, 20000 <br />
+          Sremska Mitrovica <br />
+          <span>freelancerslobodan@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, como over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
